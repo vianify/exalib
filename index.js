8c186a98795fa567db57ddfe720621b6ed388850
Vian <rollgaming6@gmail.com>
2022-10-02 19:05:17 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+__path = process.cwd();
+var favicon = require('serve-favicon');
+var express = require('express'),
+    cors = require('cors'),
+    secure = require('ssl-express-www');
+const PORT = process.env.PORT || 8080 || 5000 || 3000
+var { color } = require('./lib/color.js')
+
+var mainrouter = require('./src/index.js')
+
+var app = express()
+app.enable('trust proxy');
+app.set("json spaces",2)
+app.use(cors())
+app.use(secure)
+app.use(favicon(__path +'/public/favicon.ico'))
+app.use(express.static("public"))
+
+app.use('/', mainrouter);
+
+app.listen(PORT, () => {
+    console.log(color("Server running on port " + PORT,'green'))
+})
+
+module.exports = app
