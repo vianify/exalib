6e53d202fb67d76b0390107003b88321893487b0
Vian <rollgaming6@gmail.com>
2022-10-02 19:06:47 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Main from "./pages/Main/Main";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Main />} />
+    </Routes>
+  );
+};
+
+export default App;
