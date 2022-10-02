ae484ccd9cd1b3b24fbf186bd80e4fe5778558ea
Vian <rollgaming6@gmail.com>
2022-10-02 19:10:55 +0700

    Add files via upload

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,1 +1,11 @@
-J
+import React from "react";
+import "MainStyle.css";
+
+const Main = () => {
+    return (
+        <h1>ReactJs</h1>
+        <p>Hello, World!</p>
+    );
+};
+
+export default Main;
